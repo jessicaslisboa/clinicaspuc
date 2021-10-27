d588dd543db525e3a8ec922355422477fce97f2f
jessicaslisboa <jessicaslisboa@gmail.com>
2021-10-26 23:03:58 -0300

    agenda

--- src/br/com/clinicaspuc/rs/AgendaApi.java
+++ src/br/com/clinicaspuc/rs/AgendaApi.java
@@ -11,12 +11,12 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import br.com.clinicaspuc.model.Agenda;
 import br.com.clinicaspuc.service.AgendaService;
+import br.com.clinicaspuc.service.ConsultaService;
 
 @Path("/agenda")
 @Produces(MediaType.APPLICATION_JSON)
@@ -25,6 +25,9 @@ public class AgendaApi {
 	
 	@EJB
 	private AgendaService agendaService;
+	
+	@EJB
+	private ConsultaService consultaService;
 	
 	@GET
 	public Response getAll(){
@@ -53,18 +56,15 @@ public class AgendaApi {
 		}
 	}	
 	
-	@GET
-	@Path("/usuario/{codUsuario}")
-	public Response getPorCodigoUsuario(@QueryParam("codUsuario") int codUsuario){
-		List<Agenda> agendas = null;
-		try {
-			agendas = agendaService.oberAgendaPorUsuario(codUsuario);
-		} catch (Exception e) {
-			throw e;
-		}
-		//TODO tratar usuario ?
-		return Response.ok(agendas).build();
-	}
+	/*
+	 * @GET
+	 * 
+	 * @Path("/usuario/{codUsuario}") public Response
+	 * getPorCodigoUsuario(@PathParam("codUsuario") int codUsuario){ List<Agenda>
+	 * agendas = null; try { agendas =
+	 * agendaService.oberAgendaPorUsuario(codUsuario); } catch (Exception e) { throw
+	 * e; } return Response.ok(agendas).build(); }
+	 */
 	
 	@POST
 	public Response salvar(Agenda agenda) {
@@ -78,8 +78,22 @@ public class AgendaApi {
 	}
 	
 	@PUT
-	public void atualizar(Agenda agenda) {
-		
+	@Path("{codigo}")
+	public Response atualizar(@PathParam("codigo") int codigo,Agenda agenda) {
+		Agenda a = null;
+		try {
+			a = agendaService.obterPorId(codigo);
+			if (a != null) {
+				a = agendaService.salvar(agenda);
+			}
+		} catch (Exception e) {
+			throw e;
+		}
+		if(a !=null) {
+			return Response.ok(a).build();
+		}else {
+			return Response.status(Response.Status.NOT_FOUND).build();
+		}
 	}
 	
 	@DELETE
